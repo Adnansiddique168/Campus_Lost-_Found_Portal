8d6906c2f98f848fa1c343eb66bfbf45ebeba9d9
NAUREEN-IFTIKHAR <naureeniftikhar717@gmail.com>
2026-05-19 21:00:30 +0500

    Revert Prisma Proxy to fix local DB context errors

--- campus-portal/lib/prisma.ts
+++ campus-portal/lib/prisma.ts
@@ -2,13 +2,10 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = new Proxy({} as PrismaClient, {
-  get(target, prop) {
-    if (!globalForPrisma.prisma) {
-      globalForPrisma.prisma = new PrismaClient({ log: ['query'] });
-    }
-    return (globalForPrisma.prisma as any)[prop];
-  }
-});
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
